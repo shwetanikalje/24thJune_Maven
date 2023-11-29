94002fd0aa1ca34e320996420b4a3dea27ea8931
dell <dell@Vipul-Ghatol>
2023-11-30 00:36:09 +0530

    [Vipul]: New class Demo1 code updation.

--- src/main/java/Module1_login/Demo1.java
+++ src/main/java/Module1_login/Demo1.java
@@ -1,8 +1,9 @@
 package Module1_login;
 
-public class Demo1 {
-	public static void main(String[] args) {
-		System.out.println("Code updation");
-	}
-
+public class Demo1
+{
+   public static void main(String[] args)
+   {
+	  System.out.println("Code updation");
+   }
 }
